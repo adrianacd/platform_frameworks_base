mptcp-galaxys2/android_frameworks_base

If MPTCP send data on multiple ifaces
Thanks to Andrei Maruseac <andrei.maruseac@intel.com>

Signed-off-by: Gregory Detal <gregory.detal@uclouvain.be>

services/java/com/android/server/ConnectivityService.java
@@ -84,6 +84,7 @@ import com.google.android.collect.Lists;
 import com.google.android.collect.Sets;
 import dalvik.system.DexClassLoader;
 import java.io.FileDescriptor;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
@@ -169,6 +170,8 @@ public class ConnectivityService extends IConnectivityManager.Stub {
     private Object mDnsLock = new Object();
     private int mNumDnsEntries;
     private boolean mDnsOverridden = false;
+
+    private boolean mMultipathCapable = false;
 
     private boolean mTestMode;
     private static ConnectivityService sServiceInstance;
@@ -359,6 +362,10 @@ public class ConnectivityService extends IConnectivityManager.Stub {
         } catch (IllegalArgumentException e) {
             loge("Error setting defaultDns using " + dns);
         }
+
+	File mptcpFile = new File("/proc/net/mptcp_pm");
+	if (mptcpFile.exists())
+		mMultipathCapable = true;
 
         mContext = checkNotNull(context, "missing Context");
         mNetd = checkNotNull(netd, "missing INetworkManagementService");
@@ -1284,6 +1291,8 @@ private NetworkStateTracker makeWimaxStateTracker() {
 
     private boolean modifyRoute(String ifaceName, LinkProperties lp, RouteInfo r, int cycleCount,
             boolean doAdd, boolean toDefaultTable) {
+	if (mMultipathCapable)
+	    toDefaultTable = false;
         if ((ifaceName == null) || (lp == null) || (r == null)) {
             if (DBG) log("modifyRoute got unexpected null: " + ifaceName + ", " + lp + ", " + r);
             return false;
@@ -1787,7 +1796,7 @@ private NetworkStateTracker makeWimaxStateTracker() {
         // if this is a default net and other default is running
         // kill the one not preferred
         if (mNetConfigs[type].isDefault()) {
-            if (mActiveDefaultNetwork != -1 && mActiveDefaultNetwork != type) {
+            if (mActiveDefaultNetwork != -1 && mActiveDefaultNetwork != type && !mMultipathCapable) {
                 if ((type != mNetworkPreference &&
                         mNetConfigs[mActiveDefaultNetwork].priority >
                         mNetConfigs[type].priority) ||

adrianacd/platform_frameworks_base

services/java/com/android/server/ConnectivityService.java
@@ -264,6 +264,8 @@ public class ConnectivityService extends IConnectivityManager.Stub {
     private Object mDnsLock = new Object();
     private int mNumDnsEntries;
 
+    private boolean mMultipathCapable = false;
+
     private boolean mTestMode;
     private static ConnectivityService sServiceInstance;
 
@@ -478,6 +480,10 @@ public class ConnectivityService extends IConnectivityManager.Stub {
         } catch (IllegalArgumentException e) {
             loge("Error setting defaultDns using " + dns);
         }
+
+	File mptcpFile = new File("/proc/net/mptcp_pm");
+	if (mptcpFile.exists())
+		mMultipathCapable = true;
 
         mContext = checkNotNull(context, "missing Context");
         mNetd = checkNotNull(netManager, "missing INetworkManagementService");
@@ -1636,6 +1642,8 @@ public class ConnectivityService extends IConnectivityManager.Stub {
 
     private boolean modifyRoute(LinkProperties lp, RouteInfo r, int cycleCount, boolean doAdd,
             boolean toDefaultTable, boolean exempt) {
+        if (mMultipathCapable)
+            toDefaultTable = false;
         if ((lp == null) || (r == null)) {
             if (DBG) log("modifyRoute got unexpected null: " + lp + ", " + r);
             return false;
@@ -2261,8 +2269,9 @@ public class ConnectivityService extends IConnectivityManager.Stub {
         // if this is a default net and other default is running
         // kill the one not preferred
         if (mNetConfigs[newNetType].isDefault()) {
-            if (mActiveDefaultNetwork != -1 && mActiveDefaultNetwork != newNetType) {
+            if (mActiveDefaultNetwork != -1 && mActiveDefaultNetwork != newNetType && !mMultipathCapable) {
                 if (isNewNetTypePreferredOverCurrentNetType(newNetType)) {
+        
                     // tear down the other
                     NetworkStateTracker otherNet =
                             mNetTrackers[mActiveDefaultNetwork];